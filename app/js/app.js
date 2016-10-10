2540bb2dee8469576ec19195f12107802dcb9fcc
THBar <choreno@gmail.com>
2016-10-09 23:19:40 -0400

    event broadcast and emit

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,6 @@
     'use strict';
     angular.module('myApp', ['postsModule']) ;
 
-    angular.module('myApp').run(function($rootScope){
-        $rootScope.title = 'Famouse Books';
-    })
-
-
+   
 
 })();
--- app/modules/posts/js/controllers.js
+++ app/modules/posts/js/controllers.js
@@ -1,59 +1,59 @@
 (function () {
     'use strict';
     angular.module('posts.controllers', [])
-        .controller('FinanceController', ['$scope', FinanceController])
-        .controller('GreetingController', ['$scope', '$timeout' , GreetingContrller]);
+        // .controller('FinanceController', ['$scope', FinanceController])
+        // .controller('GreetingController', ['$scope', '$timeout' , GreetingContrller])
+        .controller('MessageController', ['$scope', '$timeout', MessageController])
+        .controller('StatsController', ['$scope', '$timeout', StatsController])
 
 
-
-
-    function Car(col, steering) {
-        this.col = col;
-        this.steering = steering;
-    }
-
-    Car.prototype.year = 2012;
-
-    var car = new Car('red', 'left');
-    console.log(car.col);
-    console.log(car.year);
-    console.log(car.hasOwnProperty('col'));
-
-
-
-    function FinanceController($scope) {
-        $scope.salary = 0;
-        $scope.percentage = 0;
-        $scope.result = function () {
-            return $scope.salary * $scope.percentage * 0.01;
-        }
-        $scope.greeting = 'Yeo!!'
-    }
-
-
-    function GreetingContrller($scope, $timeout) {
-        $scope.now = new Date();
-        $scope.greeting = 'Hello';
+    function MessageController($scope, $timeout) {
         
-        $scope.scheduleTask = function () {
+        $scope.messages = [{
+            sender: 'user1',
+            text: 'Msg1'
+        }];
 
-          $timeout(function(){
-              $scope.message = "Fetched after 3 seccccc"
-          },3000)  
-            // $scope.message = '......';
-            
-            // setTimeout(function () {
+        var timer;
+        var count = 0;
 
-            //     $scope.$apply(function () { // wrapped the code in $apply()
-            //         $scope.message = 'Fetched after 3 seconds';
-            //         console.log('message=' + $scope.message); //log this to console
-            //     });
-            //     }, 3000);
-        }
+        $scope.loadMessages = function () {
+            count++;
+            $scope.messages.push({
+                sender: 'user1',
+                text: 'Random message' + count
+            });
+
+            timer = $timeout($scope.loadMessages, 2000);
+            if (count === 3) {
+                $scope.$broadcast('EVENT_NO_DATA', 'Not Connected');
+                $timeout.cancel(timer);
+            };
+        };
+
+        timer = $timeout($scope.loadMessages, 2000);
+        $scope.$on('EVENT_RECEIVED', function () {
+            console.log('Received emitted event');
+
+        })
+
+
+
     }
 
 
+    function StatsController($scope, $timeout) {
+        $scope.name = 'World';
+        $scope.status = 'Connected';
+        $scope.statusColor = 'green';
+        $scope.$on('EVENT_NO_DATA', function (event, data) {
+            console.log('received broadcasted event');
+            $scope.status = data;
+            $scope.statusColor = 'red';
+            $scope.$emit('EVENT_RECEIVED');
+        });
 
+    }
 
 
 
