a2c7d417c02202e914f1a0c988469e0412d97a4e
THBar <choreno@gmail.com>
2016-10-13 16:56:50 -0400

    using ngform directive

--- app/js/app.js
+++ app/js/app.js
@@ -1,51 +1,54 @@
-// (function () {
-//     'use strict';
+(function () {
+    'use strict';
 
-//     //this one allows any kind of urls, but does not work, always show the /posts UI
-//     angular.module('spBlogger', ['spBlogger.posts'])
-//         .run(['$state', function ($state) {
-//             $state.go('allPosts');
-//         }
-//         ]);
-
-
-
-//     // //Which one is better ???  -- this one always shows /posts url whatever typed in url
-//     // angular.module('spBlogger', ['spBlogger.posts'])
-//     //     .config(function ($urlRouterProvider) {
-//     //         $urlRouterProvider.otherwise('/posts');
-//     //     })
+    //this one allows any kind of urls, but does not work, always show the /posts UI
+    angular.module('spBlogger', ['spBlogger.posts'])
+        .run(['$state', function ($state) {
+            $state.go('allPosts');
+        }
+        ]);
 
 
-    
 
+    // //Which one is better ???  -- this one always shows /posts url whatever typed in url
+    // angular.module('spBlogger', ['spBlogger.posts'])
+    //     .config(function ($urlRouterProvider) {
+    //         $urlRouterProvider.otherwise('/posts');
+    //     })
 
-// })();
+})();
 
-(function() {
-'use strict';
+(function(){
+    'use strict';
+    angular.module('myApp',[])
+        .controller('UserController', ['$scope', UserController]);
 
-    angular
-        .module('myApp')
-        .controller('UserController', ['$scope'], UserController);
-
-    
     function UserController($scope) {
-        var vm = this;
-        
-        $scope.user = {}; 
-        $scope.countries =[{
-            id:'US',
-            desc:'United States'
+         var vm = this;
+
+        $scope.user = {};
+        $scope.countries = [{
+            id: 'US',
+            desc: 'United States'
         },
         {
-            id:'GB',
-            desc:'United Kingdom'
+            id: 'GB',
+            desc: 'United Kingdom'
         }];
-        
+
+        $scope.saveUser = function(){
+            if($scope.myform.$valid){
+                console.log('saving user');
+                
+            }
+            else{
+                console.log('unable to save');
+                
+            }
+
+        }
 
 
     }
-
-
 })();
+
