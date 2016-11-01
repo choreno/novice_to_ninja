a4f0873e14a0ede0ec77dd6304d74432ab8e0239
THBar <choreno@gmail.com>
2016-10-31 21:51:03 -0400

    isolated scope

--- app/js/app.js
+++ app/js/app.js
@@ -3,3 +3,13 @@
 angular.module('exampleApp',[]);
 
 
+// angular.module("exampleApp", [])
+//     .directive("scopeDemo", function () {
+//         return {
+//             template:
+//             "<div class='panel-body'>Name: <input ng-model=name /></div>",
+//         }
+//     })
+//     .controller("scopeCtrl", function ($scope) {
+//         // do nothing - no behaviours required
+//     });
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,23 +1,41 @@
-(function () {
+// (function () {
+// 	'use strict';
+// 	angular.module('exampleApp')
+// 		.directive('unorderedList', [unorderedList]);
+
+// 	function unorderedList() {
+// 		return {
+// 			restrict: 'AE',
+// 			// templateUrl: function(element, attrs){ //no scope allowed???
+
+// 			// 	return attrs['template'] == 'table' ?  './TemplateViews/tableTemplate.html' : './TemplateViews/itemTemplate.html'
+// 			// },
+// 			templateUrl: './TemplateViews/tableTemplate.html',
+// 			// replace: true,
+// 			link: function (scope, element, attrs) {
+// 				scope.data = scope[attrs['unorderedList']]; //get the ctrl's scope data ???
+// 			}
+
+
+// 		}
+// 	}
+// })();
+
+
+
+(function(){
 	'use strict';
 	angular.module('exampleApp')
-		.directive('unorderedList', [unorderedList]);
+		.directive('scopeDemo', [scopeDemo]);
 
-	function unorderedList() {
-		return {
-			restrict: 'AE',
-			// templateUrl: function(element, attrs){ //no scope allowed???
-
-			// 	return attrs['template'] == 'table' ?  './TemplateViews/tableTemplate.html' : './TemplateViews/itemTemplate.html'
-			// },
-			templateUrl: './TemplateViews/tableTemplate.html',
-			// replace: true,
-			link: function (scope, element, attrs) {
-				scope.data = scope[attrs['unorderedList']]; //get the ctrl's scope data ???
-			}
-
-
+	function scopeDemo(){
+		return{
+			restrict: 'A',
+			template: function(){
+				return angular.element(document.querySelector("#scopeTemplate")).html();
+			},
+			scope: {}
+			
 		}
 	}
 })();
-
